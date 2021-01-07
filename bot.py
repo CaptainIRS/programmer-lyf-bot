eb814fa167f877db2c7a8601c89ef9171ad88dc7
Sakthi Prasath <63360785+sakthi-prasath@users.noreply.github.com>
2021-01-07 15:56:01 +0530

    Added methods for blogs and devrant and fixes (#4)
    * [Add] bot.py
    
    * Update .env.example
    
    * Update .env.example
    
    * [Add] devrant and blog delivery methods and fixes

--- bot.py
+++ bot.py
@@ -1,36 +1,40 @@
 import os
 import json
 import discord
-import random
 import schedule
+import devrant
 
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
-from datetime import date
+from datetime import date, datetime
 from reddit import reddit
+from feeds import feeds
 
-load_dotenv()
+load_dotenv(override=True)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-FUN = os.getenv('FUN')
-WEBDEV = os.getenv('WEBDEV')
-SYSAD = os.getenv('SYSADMIN')
-ANDROID = os.getenv('ANDROID')
-CAREER = os.getenv('CAREER')
-BLOGS = os.getenv('BLOGS')
-DEVRANT = os.getenv('DEVRANT')
-SLASHDOT = os.getenv('ANDROID')
+FUN = int(os.getenv('FUN'))
+WEBDEV = int(os.getenv('WEBDEV'))
+SYSAD = int(os.getenv('SYSADMIN'))
+ANDROID = int(os.getenv('ANDROID'))
+CAREER = int(os.getenv('CAREER'))
+BLOGS = int(os.getenv('BLOGS'))
+DEVRANT = int(os.getenv('DEVRANT'))
+SLASHDOT = int(os.getenv('ANDROID'))
 
 bot = commands.Bot(command_prefix='./')
-jsonDict = reddit.result
+redditDict = reddit.result
+blogDict = feeds.new_array
 
-
-def create_reddit_embed(subreddit_name, 
-                        title, selftext, url, 
+def create_reddit_embed(subreddit, 
+                        title, selftext, url, image_type, 
                         image_url, subreddit_url, 
                         subreddit_icon):
     if selftext == "":
         selftext = "\u200b"
+
+    if len(selftext) > 2044:
+        selftext = selftext[:2044] + '...'
     
     embed = discord.Embed(
         title = title,
@@ -39,95 +43,151 @@ def create_reddit_embed(subreddit_name,
         colour = discord.Colour.red()
     )
 
-    if image_url != "":
+    if image_url != "" and image_type == "image":
         embed.set_image(url = image_url)
 
     embed.set_author(
-        name = subreddit_name, 
+        name = subreddit, 
         url = subreddit_url, 
         icon_url = subreddit_icon
         )
 
     return embed
 
+def create_blog_embed(author_name, title, description, url, image_url):
+    if description == "":
+        description = "\u200b"
+
+    if len(description) > 2044:
+        selftext = description[:2044] + '...'
+    
+    embed = discord.Embed(
+        title = title,
+        description = description,
+        url = url,
+        colour = discord.Colour.blue()
+    )
+
+    if image_url != "":
+        embed.set_image(url = image_url)
+
+    embed.set_author(name = author_name, icon_url = 'https://online.usm.edu/wp-content/uploads/2018/01/online-engineering-technology-blog-image.jpg')
+
+    return embed
+
 @bot.event
 async def on_ready():
-    schedule.every().monday.do(reddit_weekly.start())
-    schedule.every().day.at("00:00").do(reddit_daily.start())
-    schedule.every().day.at("00:00").do(reddit_monthly.start())
+    reddit_weekly.start()
+    reddit_daily.start()
+    reddit_monthly.start()
+    fetch_devrant.start()
+    fetch_blog.start()
 
-@tasks.loop(hours=168)
+@tasks.loop(hours=24)
 async def reddit_weekly():
-    for subreddit in jsonDict["subreddits"]:
+    if datetime.today().weekday() != 0:
+        return
+
+    for subreddit in redditDict["subreddits"]:
         if subreddit["frequency"] == "weekly":
-            if subreddit["subreddit_name"] == "webdev":
+            if subreddit["subreddit"] == "webdev":
                 channel = bot.get_channel(WEBDEV)
-            elif subreddit["subreddit_name"] == "sysadmin":
+            elif subreddit["subreddit"] == "sysadmin":
                 channel = bot.get_channel(SYSAD)
-            elif subreddit["subreddit_name"] == "androiddev":
+            elif subreddit["subreddit"] == "androiddev":
                 channel = bot.get_channel(ANDROID)
-            elif subreddit["subreddit_name"] == "ITCareerQuestions":
+            elif subreddit["subreddit"] == "ITCareerQuestions":
                 channel = bot.get_channel(CAREER)
             else:
                 channel = bot.get_channel(FUN)
             for post in subreddit["posts"]:
-                embed = create_reddit_embed(subreddit["subreddit_name"],
-                    post["title"], post["selftext"], post["url"], post["image"]["url"],
+                embed = create_reddit_embed(subreddit["subreddit"],
+                    post["title"], post["selftext"], post["url"], post["media"]["medium"], post["media"]["url"],
                     subreddit["subreddit_url"], subreddit["subreddit_icon"])
-                await channel.send(embed = embed)
+                try:
+                    await channel.send(embed = embed)
+                except:
+                    pass
     
-    return schedule.CancelJob
 
-@tasks.loop(hours=168)
-async def devrant():
-    #fetch from devrant
-    pass
+@tasks.loop(hours=24)
+async def fetch_devrant():
+    if datetime.today().weekday() != 0:
+        return
+
+    channel = bot.get_channel(DEVRANT)
+    for embed in devrant.get_rants('weekly','10'):
+        try:
+            await channel.send(embed = embed)
+        except:
+            pass
 
 @tasks.loop(hours=24)
 async def reddit_monthly():
     if date.today().day != 1:
         return
 
-    for subreddit in jsonDict["subreddits"]:
+    for subreddit in redditDict["subreddits"]:
         if subreddit["frequency"] == "monthly":
-            if subreddit["subreddit_name"] == "webdev":
+            if subreddit["subreddit"] == "webdev":
                 channel = bot.get_channel(WEBDEV)
-            elif subreddit["subreddit_name"] == "sysadmin":
+            elif subreddit["subreddit"] == "sysadmin":
                 channel = bot.get_channel(SYSAD)
-            elif subreddit["subreddit_name"] == "androiddev":
+            elif subreddit["subreddit"] == "androiddev":
                 channel = bot.get_channel(ANDROID)
-            elif subreddit["subreddit_name"] == "ITCareerQuestions":
+            elif subreddit["subreddit"] == "ITCareerQuestions":
                 channel = bot.get_channel(CAREER)
             else:
                 channel = bot.get_channel(FUN)
             for post in subreddit["posts"]:
-                embed = create_reddit_embed(subreddit["subreddit_name"],
-                    post["title"], post["selftext"], post["url"], post["image"]["url"],
+                embed = create_reddit_embed(subreddit["subreddit"],
+                    post["title"], post["selftext"], post["url"], post["media"]["medium"], post["media"]["url"],
                     subreddit["subreddit_url"], subreddit["subreddit_icon"])
-                await channel.send(embed = embed)
+                try:
+                    await channel.send(embed = embed)
+                except:
+                    pass
 
-    return schedule.CancelJob
 
-@tasks.loop(hours=24)
+@tasks.loop(hours=1)
 async def reddit_daily():
-    for subreddit in jsonDict["subreddits"]:
+    if datetime.today().hour != 0:
+        return
+
+    for subreddit in redditDict["subreddits"]:
         if subreddit["frequency"] == "daily":
-            if subreddit["subreddit_name"] == "webdev":
+            if subreddit["subreddit"] == "webdev":
                 channel = bot.get_channel(WEBDEV)
-            elif subreddit["subreddit_name"] == "sysadmin":
+            elif subreddit["subreddit"] == "sysadmin":
                 channel = bot.get_channel(SYSAD)
-            elif subreddit["subreddit_name"] == "androiddev":
+            elif subreddit["subreddit"] == "androiddev":
                 channel = bot.get_channel(ANDROID)
-            elif subreddit["subreddit_name"] == "ITCareerQuestions":
+            elif subreddit["subreddit"] == "ITCareerQuestions":
                 channel = bot.get_channel(CAREER)
             else:
                 channel = bot.get_channel(FUN)
             for post in subreddit["posts"]:
-                embed = create_reddit_embed(subreddit["subreddit_name"],
-                    post["title"], post["selftext"], post["url"], post["image"]["url"],
+                embed = create_reddit_embed(subreddit["subreddit"],
+                    post["title"], post["selftext"], post["url"], post["media"]["medium"], post["media"]["url"],
                     subreddit["subreddit_url"], subreddit["subreddit_icon"])
-                await channel.send(embed = embed)
+                try:
+                    await channel.send(embed = embed)
+                except:
+                    pass
     
-    return schedule.CancelJob
+@tasks.loop(hours=1)
+async def fetch_blog():
+    if datetime.today().hour != 0:
+        return
+
+    channel = bot.get_channel(BLOGS)
+    for post in blogDict:
+        embed = create_blog_embed(post["author_name"],
+                post["title"], post["description"], post["url"], post["image"],
+                )
+        try:
+            await channel.send(embed = embed)
+        except:
+            pass
 
 bot.run(TOKEN)
