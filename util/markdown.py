bb84930f180300a0bba8d586cfab9955674fedbf
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2021-01-09 11:44:48 +0530

    lint fixes

--- util/markdown.py
+++ util/markdown.py
@@ -6,12 +6,14 @@ from markdownify import MarkdownConverter
 
 
 class _CustomMarkdownConverter(MarkdownConverter):
-    def convert_hn(self, n, el, text, convert_as_inline):
+    def convert_hn(self, _, __, text, convert_as_inline):
+        '''Convert headings to bold markdown'''
         if convert_as_inline:
             return text
         return '**%s** \n\n' % text
 
-    def convert_img(self, el, text, convert_as_inline):
+    def convert_img(self, _, __, ___):
+        '''Ignore images'''
         return ''
 
 
